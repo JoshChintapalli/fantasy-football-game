98f885060dfec930df8ae1f3aeb64ef9c8101036
Joshua Chintapalli <josh.chintapalli@gmail.com>
2026-06-19 21:43:20 -0400

    Add bonus round and update the selecting phase to function during the bonus round. Fix bugs related to position checks and replace buttons

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from game_engine import initialize_game, spin_wheel, get_best_players, update_roster, calculate_score
+from game_engine import initialize_game, spin_wheel, get_best_players, update_roster, calculate_score, slot_to_position
 
 
 #init — game hasn't started yet
@@ -18,7 +18,7 @@ if "week" not in st.session_state:
 if "roster" not in st.session_state:
     st.session_state.roster = None
 if "df" not in st.session_state:
-    st.session_state.df = pd.read_csv("app_data.csv")
+    st.session_state.df = None
 if "teams" not in st.session_state:
     st.session_state.teams = None
 if "current_team" not in st.session_state:
@@ -27,6 +27,10 @@ if "game_phase" not in st.session_state:
     st.session_state.game_phase = "init"
 if "current_selection" not in st.session_state:
     st.session_state.current_selection = None
+if "is_bonus" not in st.session_state:
+    st.session_state.is_bonus = False
+if "full_df" not in st.session_state:
+    st.session_state.full_df = pd.read_csv("app_data.csv")
 
 
 if st.session_state.week is not None:
@@ -37,12 +41,14 @@ if st.session_state.week is not None:
             st.write(f"{pos}: Empty")
         else:
             st.write(f"{pos}: {val['player_display_name']} - {val['fantasy_points_ppr']} pts")
+    if st.session_state.is_bonus == True:
+        st.title("Bonus Round")
 
 if st.session_state.game_phase == "init":
     st.title("Fantasy Football Game")
     st.write("Build a roster of NFL players from team wheel spins to total 200 fantasy football points")
     if st.button("Start Game"):
-        st.session_state.season, st.session_state.week, st.session_state.df, st.session_state.teams, st.session_state.roster = initialize_game(st.session_state.df)
+        st.session_state.season, st.session_state.week, st.session_state.df, st.session_state.teams, st.session_state.roster = initialize_game(st.session_state.full_df)
         st.session_state.game_phase = "spinning"
         st.rerun()
 
@@ -53,32 +59,73 @@ elif st.session_state.game_phase == "spinning":
         st.rerun()
 
 elif st.session_state.game_phase == "selecting":
-    st.title("Player Selection")
+    if st.session_state.is_bonus == False:
+        st.title("Player Selection")
     st.write("Selected Team: ", st.session_state.current_team)
-    best_selection = get_best_players(st.session_state.roster, st.session_state.current_team, st.session_state.df)
+    if st.session_state.is_bonus == True:
+        empty_roster = {
+        "QB" : None,
+        "RB1" : None,
+        "RB2" : None,
+        "WR1" : None,
+        "WR2" : None,
+        "TE" : None,
+        "FLEX" : None
+    }
+        best_selection = get_best_players(empty_roster, st.session_state.current_team, st.session_state.df)
+    else:
+        best_selection = get_best_players(st.session_state.roster, st.session_state.current_team, st.session_state.df)
+    if best_selection == False:
+        st.session_state.game_phase = "spinning"
+        st.rerun()
     if st.session_state.current_selection is None:
         for position, stats in best_selection.items():
-            st.write(position, stats)
-            if st.button("Select Player", key = position):
-                st.session_state.current_selection = position
-                st.rerun()
+            if st.session_state.is_bonus == False or any(
+                (slot_to_position[slot] == position or (slot == "FLEX" and position in slot_to_position[slot])) and stats["fantasy_points_ppr"] > val["fantasy_points_ppr"] for slot, val in st.session_state.roster.items()
+            ):
+                st.write(position, stats)
+                if st.button("Select Player", key = position):
+                    st.session_state.current_selection = position
+                    st.rerun()
     else:
         for pos, val in st.session_state.roster.items():
             col1, col2, col3 = st.columns([1, 1, 5])
+            button_name = None
             with col1:
                 st.write(f"**{pos}**")
             with col2:
                 st.write(":")
             with col3:
-                if val is not None:
+                if val is not None and st.session_state.is_bonus == False:
                     st.write(val)
-                else:
-                    if st.button(" ", key = pos):
-                        vaild_update = update_roster(st.session_state.roster, best_selection, st.session_state.current_selection, pos)
-                        if vaild_update == True:
+                elif val is None or st.session_state.is_bonus == True:
+                    if st.session_state.is_bonus == True:
+                        st.write(val)
+                        if (best_selection[st.session_state.current_selection]['fantasy_points_ppr'] > st.session_state.roster[pos]['fantasy_points_ppr']) and (slot_to_position[pos] == st.session_state.current_selection or (pos == "FLEX" and st.session_state.current_selection in slot_to_position[pos])):
+                            button_name = "Replace"
+                    else:
+                        button_name = "Select"
+                    if button_name is not None and st.button(button_name, key = pos):
+                        valid_update = update_roster(st.session_state.roster, best_selection, st.session_state.current_selection, pos, st.session_state.is_bonus)
+                        if valid_update == True:
+                            if st.session_state.is_bonus == True:
+                                st.session_state.game_phase = "complete"
+                            else:
+                                if (all(value is not None for value in st.session_state.roster.values())):
+                                    st.session_state.game_phase = "bonus"
+                                else:
+                                    st.session_state.game_phase = "spinning"
                             st.session_state.current_selection = None
-                            st.session_state.game_phase = "spinning"
                             st.rerun()
+elif st.session_state.game_phase == "bonus":
+    st.title("Bonus Round")
+    st.session_state.is_bonus = True
+    if st.button("Start Bonus Round"):
+        st.session_state.season, st.session_state.week, st.session_state.df, st.session_state.teams, empty_roster = initialize_game(st.session_state.full_df)
+        st.session_state.game_phase = "spinning"
+        st.rerun()
+
+
 
 
 
